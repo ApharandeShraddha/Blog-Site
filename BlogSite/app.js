ca724eb4b411cce18aa43650d25510bb1a4c5330
Shraddha <shraddha14488@gmail.com>
2017-10-13 01:33:39 +0000

    New and Create methods added

--- BlogSite/app.js
+++ BlogSite/app.js
@@ -11,7 +11,7 @@ mongoose.connect("mongodb://localhost/restful-blog-app" , {useMongoClient: true}
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true }));
-app.use(express.static("public"));
+
 
 //Database Schema configuration
 var blogSchema = new mongoose.Schema({
@@ -27,6 +27,8 @@ var Blog= mongoose.model("Blog" ,blogSchema);
 
 //ROUTES
 
+//INDEX Route
+
 app.get("/blogs",function(req , res){
        Blog.find({},function(err , blogs){
          if(err){
@@ -38,11 +40,55 @@ app.get("/blogs",function(req , res){
      
     });
 
+//NEW Route
 
+app.get("/blogs/new", function(req, res){
+   res.render("new"); 
+});
+
+//CREATE Route
+
+app.post("/blogs", function(req, res){
+   // req.body.blog.body = req.sanitize(req.body.blog.body);
+   var formData = req.body.blog;
+   Blog.create(formData, function(err, newBlog){
+       console.log(newBlog);
+      if(err){
+          res.render("new");
+      } else {
+          res.redirect("/blogs");
+      }
+   });
+});
+
+//SHOW route
+app.get("/blogs/:id", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+      if(err){
+          res.redirect("/");
+      } else {
+          res.render("show", {blog: foundBlog});
+      }
+   });
+});
+
+//EDIT route
+app.get("/blogs/:id/edit", function(req, res){
+   Blog.findById(req.params.id, function(err, blog){
+       if(err){
+           console.log(err);
+           res.redirect("/")
+       } else {
+           res.render("edit", {blog: blog});
+       }
+   });
+});
+
+//route to main page
 app.get("/",function(req , res){
    
        res.redirect("/blogs");
-    });
+});
     
 //Listen on cloud 9 port
 app.listen(process.env.PORT , process.env.IP,function(){
